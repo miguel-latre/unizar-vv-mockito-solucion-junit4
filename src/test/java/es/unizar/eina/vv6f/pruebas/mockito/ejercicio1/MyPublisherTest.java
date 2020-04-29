128b465b5e3be20006f510b2c2199896253f9fba
Miguel Latre <latre@unizar.es>
2020-04-29 13:59:52 +0200

    Añadido el test correspondiente a dos mocks y dos mensajes, pero entre la suscripción del primer y el segundo mock, el Publisher publica el primer mensaje

--- src/test/java/es/unizar/eina/vv6f/pruebas/mockito/ejercicio1/MyPublisherTest.java
+++ src/test/java/es/unizar/eina/vv6f/pruebas/mockito/ejercicio1/MyPublisherTest.java
@@ -59,6 +59,7 @@ public class MyPublisherTest {
 
         // Creación del objeto de pruebas
         Publisher publisher = new MyPublisher();
+
         // Configuración del objeto de pruebas para que utilice los mocks:
         // suscripción de los mocks al objeto de pruebas
         publisher.add(subscriber1);
@@ -71,6 +72,35 @@ public class MyPublisherTest {
         // Comprobación del estado de los mocks
         verify(subscriber1).receive(eq(FIRST_MESSAGE));
         verify(subscriber2).receive(eq(FIRST_MESSAGE));
+        verify(subscriber1).receive(eq(SECOND_MESSAGE));
+        verify(subscriber2).receive(eq(SECOND_MESSAGE));
+    }
+
+    @Test
+    public void test_2subscribers_2messages_interleaved() {
+        // Creación de los mocks
+        Subscriber subscriber1 = Mockito.mock(Subscriber.class);
+        Subscriber subscriber2 = Mockito.mock(Subscriber.class);
+
+        // Creación del objeto de pruebas
+        Publisher publisher = new MyPublisher();
+
+        // Configuración del objeto de pruebas para que utilice los mocks:
+        // suscripción del primer mocks al objeto de pruebas
+        publisher.add(subscriber1);
+
+        // Ejecución del objeto de pruebas, enviado el primer mensaje solo al primer mock
+        publisher.publish(FIRST_MESSAGE);
+
+        // Configuración del objeto de pruebas para que utilice los mocks:
+        // suscripción del segundo mock al objeto de pruebas tras mandar el primer mensaje
+        publisher.add(subscriber2);
+
+        // Ejecución del objeto de pruebas, enviado el segundo mensaje a los dos mocks
+        publisher.publish(SECOND_MESSAGE);
+
+        // Comprobación del estado de los mocks
+        verify(subscriber1).receive(eq(FIRST_MESSAGE));
         verify(subscriber1).receive(eq(SECOND_MESSAGE));
         verify(subscriber2).receive(eq(SECOND_MESSAGE));
     }
