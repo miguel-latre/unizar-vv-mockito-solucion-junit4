ac7ded843564992f179660fac4a24d8839c514ef
Miguel Latre <latre@unizar.es>
2020-04-29 13:54:28 +0200

    Añadido el test correspondiente a dos mocks y un mensaje a ambos

--- src/test/java/es/unizar/eina/vv6f/pruebas/mockito/ejercicio1/MyPublisherTest.java
+++ src/test/java/es/unizar/eina/vv6f/pruebas/mockito/ejercicio1/MyPublisherTest.java
@@ -27,4 +27,26 @@ public class MyPublisherTest {
         // Comprobación del estado del mock
         verify(subscriber).receive(eq(FIRST_MESSAGE));
     }
+
+    @Test
+    public void test_2subscribers_1message() {
+        // Creación de los mocks
+        Subscriber subscriber1 = Mockito.mock(Subscriber.class);
+        Subscriber subscriber2 = Mockito.mock(Subscriber.class);
+
+        // Creación del objeto de pruebas
+        Publisher publisher = new MyPublisher();
+
+        // Configuración del objeto de pruebas para que utilice los mocks:
+        // suscripción de los mocks al objeto de pruebas
+        publisher.add(subscriber1);
+        publisher.add(subscriber2);
+
+        // Ejecución del objeto de pruebas. Se supone que este debe interactuar con los mocks.
+        publisher.publish(FIRST_MESSAGE);
+
+        // Comprobación del estado de los mocks
+        verify(subscriber1).receive(eq(FIRST_MESSAGE));
+        verify(subscriber2).receive(eq(FIRST_MESSAGE));
+    }
 }
