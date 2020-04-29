5082d8087dee2477048fefc29e11e6ab2b62542a
Miguel Latre <latre@unizar.es>
2020-04-29 13:56:26 +0200

    Añadido el test correspondiente a dos mocks y dos mensajes distintos a ambos

--- src/test/java/es/unizar/eina/vv6f/pruebas/mockito/ejercicio1/MyPublisherTest.java
+++ src/test/java/es/unizar/eina/vv6f/pruebas/mockito/ejercicio1/MyPublisherTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.verify;
 
 public class MyPublisherTest {
     private  static final String FIRST_MESSAGE = "1st message";
+    private static final String SECOND_MESSAGE = "2nd message";
 
     @Test
     public void test_1subscriber_1message() {
@@ -49,4 +50,29 @@ public class MyPublisherTest {
         verify(subscriber1).receive(eq(FIRST_MESSAGE));
         verify(subscriber2).receive(eq(FIRST_MESSAGE));
     }
+
+    @Test
+    public void test_2subscribers_2messages() {
+        // Creación de los mocks
+        Subscriber subscriber1 = Mockito.mock(Subscriber.class);
+        Subscriber subscriber2 = Mockito.mock(Subscriber.class);
+
+        // Creación del objeto de pruebas
+        Publisher publisher = new MyPublisher();
+        // Configuración del objeto de pruebas para que utilice los mocks:
+        // suscripción de los mocks al objeto de pruebas
+        publisher.add(subscriber1);
+        publisher.add(subscriber2);
+
+        // Ejecución del objeto de pruebas, enviado ahora dos mensajes
+        publisher.publish(FIRST_MESSAGE);
+        publisher.publish(SECOND_MESSAGE);
+
+        // Comprobación del estado de los mocks
+        verify(subscriber1).receive(eq(FIRST_MESSAGE));
+        verify(subscriber2).receive(eq(FIRST_MESSAGE));
+        verify(subscriber1).receive(eq(SECOND_MESSAGE));
+        verify(subscriber2).receive(eq(SECOND_MESSAGE));
+    }
+
 }
